5edcd31d079e71ea3e14e4d0c4f56ce63bdb8bda
S. Brent Faulkner <brentf@unwwwired.net>
2010-03-03 14:09:12 -0500

    px unit on size

--- javascripts/jquery.token-field.js
+++ javascripts/jquery.token-field.js
@@ -99,7 +99,7 @@
       var id = $(this).attr('id');
       var name = $(this).attr('name');
       var klass = $(this).attr('class');
-      var style = 'min-height: '+$(this).height()+'; height: auto !important; height: '+$(this).height()+'; width: '+$(this).width();
+      var style = 'min-height: '+$(this).height()+'px; height: auto !important; height: '+$(this).height()+'px; width: '+$(this).width()+'px;';
       var tokens = $.map($(this).val().replace(/^\s*(.+)\s*$/,'$1').split(','), function(v) { if (isToken(v)) return tokenHtml(v); console_log('Warning: ignoring bad token - '+v); return null; });
       if (settings.max > 0 && tokens.length > settings.max) {
         console_log('Warning: ignoring extra tokens after maximum of '+settings.max);
